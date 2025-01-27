c6486352afb54677300a9846b4f46a033e17c396
syoyu <ryasyo1224@gmail.com>
2025-01-28 01:47:19 +0900

    rename

--- src/SLS/subplugins/SubPluginInitializer.php
+++ src/SLS/subplugins/SubPluginInitializer.php
@@ -13,14 +13,14 @@ class SubPluginInitializer{
 	/**
 	 * @var Listener[]
 	 */
-	private static array $loadedPlugin;
+	private static array $loadPlugin;
 
 	public static function init(Main $main){
-		self::$loadedPlugin = [
+		self::$loadPlugin = [
 			new JoinMessage(),
 		];
 
-		foreach(self::$loadedPlugin as $plugin){
+		foreach(self::$loadPlugin as $plugin){
 			$plugin->onLoad();
 			foreach($plugin->getEvents() as $event){
 				$main->getServer()->getPluginManager()->registerEvents($event, $main);
